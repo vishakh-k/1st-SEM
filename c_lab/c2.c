884cc09b855e0db4153f9b187f6e9fe0fa182d21
vishakh k t <vishakhkt.2003@gmail.com>
2025-03-05 16:21:39 +0530

    second program cooked

--- c_lab/c2.c
+++ c_lab/c2.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int binarySearch(int arr[],int low,int high,int target){
     if(low>high){
         return -1;
@@ -8,27 +7,27 @@ int binarySearch(int arr[],int low,int high,int target){
     if(arr[mid]==target){
         return mid;
     }else if(arr[mid]<target){
-        return binarySearch(arr,mid+1,high,target);
+        return binarySearch(arr,mid-1,high,target);
     }else if(arr[mid]>target){
-        return binarySearch(arr,low,mid-1,target);
+        return binarySearch(arr,low,mid+1,target);
     }
 }
 int main(){
-    int n,i,target;
-    printf("Enter the number of elements");
+    int i,n,target;
+    printf("Enter the number of elements:");
     scanf("%d",&n);
     int arr[n];
-    printf("Enter the %d elements in sorted order",n);
+    printf("Enter the %d elements:",n);
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    printf("Enter the target value");
+    printf("Enter the target value:");
     scanf("%d",&target);
     int result=binarySearch(arr,0,n-1,target);
     if(result!=-1){
         printf("Element found at the index %d",result);
     }else{
-        printf("Element not found");
+        printf("element not found");
     }
     return 0;
 }
