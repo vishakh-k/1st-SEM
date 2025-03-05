49e3858ce8fe1b28a4bc4dcc01636262630fda48
vishakh k t <vishakhkt.2003@gmail.com>
2025-03-05 16:58:39 +0530

    third program cooked

--- c_lab/c2.c
+++ c_lab/c2.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
-int binarySearch(int arr[],int low,int high,int target){
-    if(low>high){
+int binarySearch(int arr[], int low, int high, int target) {
+    if (low > high) {
         return -1;
     }
-    int mid=low+(high-low)/2;
-    if(arr[mid]==target){
+    int mid = low + (high - low) / 2;
+
+    if (arr[mid] == target) {
         return mid;
-    }else if(arr[mid]<target){
-        return binarySearch(arr,mid-1,high,target);
-    }else if(arr[mid]>target){
-        return binarySearch(arr,low,mid+1,target);
+    } else if (arr[mid] < target) {
+        return binarySearch(arr, mid + 1, high, target);
+    } else {
+        return binarySearch(arr, low, mid - 1, target);
     }
 }
-int main(){
-    int i,n,target;
-    printf("Enter the number of elements:");
-    scanf("%d",&n);
+int main() {
+    int n, i, target, result;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
     int arr[n];
-    printf("Enter the %d elements:",n);
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    printf("Enter %d elements (in sorted order):\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    printf("Enter the target value:");
-    scanf("%d",&target);
-    int result=binarySearch(arr,0,n-1,target);
-    if(result!=-1){
-        printf("Element found at the index %d",result);
-    }else{
-        printf("element not found");
+    printf("Enter the number to search: ");
+    scanf("%d", &target);
+    result = binarySearch(arr, 0, n - 1, target);
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found in the array.\n");
     }
     return 0;
 }
---
+++ c_lab/c3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int bubbleSort(int arr[],int n){
+    int i,j,temp;
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+int main(){
+    int n;
+    printf("Enter the number of elements");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the %d elements",n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    bubbleSort(arr,n);
+    printf("Array after sorting \n");
+    for(int i=0;i<n;i++){
+        printf(" %d ",arr[i]);
+    }
+    return 0;
+}
